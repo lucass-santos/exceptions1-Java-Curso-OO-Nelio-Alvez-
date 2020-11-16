c8338752c1624790156039e815be56ec15f98b55
Lucas <lucas.ramuh@gmail.com>
2020-11-16 12:39:13 -0300

    bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -6,15 +6,15 @@ import java.util.concurrent.TimeUnit;
 
 public class Reservation {
 	private Integer roomNumber;
-	private Date checkin;
-	private Date checkout;
+	private Date checkIn;
+	private Date checkOut;
 	
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
-	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
 		this.roomNumber = roomNumber;
-		this.checkin = checkin;
-		this.checkout = checkout;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
 	}
 
 	public Integer getRoomNumber() {
@@ -26,27 +26,36 @@ public class Reservation {
 	}
 
 	public Date getCheckin() {
-		return checkin;
+		return checkIn;
 	}
 
 	public Date getCheckout() {
-		return checkout;
+		return checkOut;
 	}
 
 	
 	public long duration() {
-		long diff = checkout.getTime() - checkin.getTime() ;
+		long diff = checkOut.getTime() - checkIn.getTime() ;
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public void updateDates(Date checkin, Date checkout) {
-		this.checkin = checkin;
-		this.checkout = checkout;
+	public String updateDates(Date checkin, Date checkout) {
+		
+		Date now = new Date();
+		if(checkIn.before(now) ||checkOut.before(now)) {
+			return "Error in reservation: Reservation dates for update must be future dates";	
+		}
+		if(!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date";
+		}
+		this.checkIn = checkin;
+		this.checkOut = checkout;
+		return null;
 	}
 	
 	@Override
 	public String toString() {
-		return String.format("Reservation: Room %d, check-in: %s, check-out: %s, %d nights", roomNumber, sdf.format(checkin), sdf.format(checkout), duration());
+		return String.format("Reservation: Room %d, check-in: %s, check-out: %s, %d nights", roomNumber, sdf.format(checkIn), sdf.format(checkOut), duration());
 	}
 	
 	
